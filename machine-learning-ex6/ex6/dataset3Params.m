4315d493a2e0b03ecbc9f20c588538afb8e11620
unknown <>
2017-01-10 11:08:17 +0800

    ex6

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,10 +23,27 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+param = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+n = length(param);
+min_error = inf; 
 
-
-
-
+for i = 1:n
+    for j = 1:n
+        temp_C = param(i);
+        temp_sigma = param(j);
+        
+        model = svmTrain(X, y, temp_C, @(x1, x2) gaussianKernel(x1, x2, temp_sigma));
+        
+        predictions = svmPredict(model, Xval);
+        error_val = mean(double(predictions ~= yval));
+        
+        if error_val < min_error
+            C = temp_C;
+            sigma = temp_sigma;
+            min_error = error_val;
+        end
+    end
+end
 
 
 % =========================================================================
